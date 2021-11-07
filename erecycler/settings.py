b41726f10c7c0a31087d0d72b391df407f8a23b8
ahn1305 <ahnashwin1305@gmail.com>
2021-11-07 18:43:26 +0530

    latest commit

--- erecycler/settings.py
+++ erecycler/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'crispy_forms',
-    'bootstrap_modal_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- users/views.py
+++ users/views.py
@@ -264,7 +264,7 @@ def FImageVerify(request):
             # determined by the first position in the shape tuple, in this case 1.
             data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
             # Replace this with the path to your image
-            image = Image.open(inpt).convert('RGB')
+            image = Image.open(inpt).convert("RGB")
             #resize the image to a 224x224 with the same strategy as in TM2:
             #resizing the image to be at least 224x224 and then cropping from the center
             size = (224, 224)
